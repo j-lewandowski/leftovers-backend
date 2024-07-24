d9203daa8fc6d94eb5c114378b9685a417cf7107
Jakub <kubalew.pl@gmail.com>
2024-07-24 10:43:58 +0200

    chore: changed signup route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,12 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UserDto } from './dto/user.dto';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { UserDto } from 'src/users/dto/user.dto';
+import { UsersService } from 'src/users/users.service';
 
-@Controller('users')
-export class UsersControler {
+@Controller('auth')
+export class AuthController {
   constructor(private usersService: UsersService) {}
-  @Post()
+  @Post('signup')
   async registerUser(@Body() userData: CreateUserDto): Promise<UserDto> {
     return this.usersService.registerUser(userData);
   }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersRepository } from './users.repository';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { UsersService } from './users.service';
-import { UsersControler } from './users.controller';
 
 @Module({
   imports: [PrismaModule],
   providers: [UsersRepository, UsersService],
-  controllers: [UsersControler],
   exports: [UsersService],
 })
 export class UsersModule {}
