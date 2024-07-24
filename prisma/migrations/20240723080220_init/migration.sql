6a1e1e2d29574d73870f79a9bcd7ed49f2ea81f3
Jakub <kubalew.pl@gmail.com>
2024-07-24 08:59:27 +0200

    feat: changed user model

--- prisma/migrations/20240723080220_init/migration.sql
+++ prisma/migrations/20240723080220_init/migration.sql
@@ -7,10 +7,6 @@ CREATE TABLE "User" (
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
-INSERT INTO "User"(email, name) VALUES ('test1@email.com', 'testname1');
-INSERT INTO "User"(email, name) VALUES ('test2@email.com', 'testname2');
-INSERT INTO "User"(email, name) VALUES ('test3@email.com', 'testname3');
-
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
