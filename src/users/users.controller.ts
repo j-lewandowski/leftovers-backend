3f9e6533bed7758c9e0a27cfb1c830ff714131e9
Jakub <kubalew.pl@gmail.com>
2024-07-26 14:56:34 +0200

    feat: added access token authentication

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,7 +2,11 @@ import { Controller, Get, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { UsersEmailsResponseDto } from './dto/users-email-reponse.dto';
+<<<<<<< HEAD
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+=======
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+>>>>>>> 4d760c8 (feat: added access token authentication)
 
 @ApiTags('users')
 @Controller()
