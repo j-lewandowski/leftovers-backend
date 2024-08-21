9dc951d0b7436057941c3e1dc6a8ef0985dec07d
Jakub <kubalew.pl@gmail.com>
2024-08-21 10:55:39 +0200

    docs: add documentation for the upload file endpoint

--- src/upload-file/dto/GetSignedUrl.dto.ts
+++ src/upload-file/dto/GetSignedUrl.dto.ts
@@ -1,8 +1,16 @@
+import { faker } from '@faker-js/faker';
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
 
 export class GetSignedUrlDto {
   @IsNotEmpty()
+  @ApiProperty({
+    example: faker.string.uuid(),
+  })
   userId: string;
   @IsNotEmpty()
+  @ApiProperty({
+    example: 'Spaghetti Bolognese',
+  })
   recipeTitle: string;
 }
--- src/upload-file/upload-file.controller.ts
+++ src/upload-file/upload-file.controller.ts
@@ -2,11 +2,22 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { UploadFileService } from './upload-file.service';
 import { GetSignedUrlDto } from './dto/GetSignedUrl.dto';
 import { SignedUrlResponseDto } from './dto/SignedUrlResponse.dto';
+import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller('upload-file')
+@ApiTags('upload file')
 export class UploadFileController {
   constructor(private readonly uploadFileService: UploadFileService) {}
 
+  @ApiOperation({
+    summary:
+      'Allows to generate signed up url that can be used to upload images to AWS S3.',
+  })
+  @ApiBody({
+    description:
+      'User id and recipe title will be used to generate object key that can be used to obtain the image later.',
+    type: GetSignedUrlDto,
+  })
   @Post()
   async getSignedUrl(
     @Body() { userId, recipeTitle }: GetSignedUrlDto,
