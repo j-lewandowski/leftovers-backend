5bb9273d1a82a1a3cdfc919e21d56a1a8f1b1754
Jakub Lewandowski <63073718+j-lewandowski@users.noreply.github.com>
2024-12-16 11:19:42 +0100

    chore: move api docs to /api

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config, {});
 
-  SwaggerModule.setup('/', app, document, {
+  SwaggerModule.setup('/api', app, document, {
     swaggerOptions: {
       authAction: {
         'basic-auth': {
